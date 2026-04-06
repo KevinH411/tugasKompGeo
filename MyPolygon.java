9464b9c32a00aff23c265d6d38349f2b5938b0d6
0fflines <6182201102@student.unpar.ac.id>
2026-04-07 03:04:26 +0700

    fungsi no 7

--- MyPolygon.java
+++ MyPolygon.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.text.Position;
+
 public class MyPolygon {
     /**  */
     List<MyPoint> Points;
 
     /**
      * konstruktor
+     * 
      * @return
      */
     public MyPolygon() {
@@ -15,6 +18,7 @@ public class MyPolygon {
 
     /**
      * [5a,6a,7a] masukin titik
+     * 
      * @param p
      * @return
      */
@@ -24,33 +28,46 @@ public class MyPolygon {
 
     /**
      * [5b] konveks ato bukan
+     * 
      * @return
      */
     boolean isConvex() {
-		boolean convex = true;
-		
+        boolean convex = true;
+
         return convex;
     }
 
     /**
      * [6b] luas poligon ini
+     * 
      * @return
      */
     double area() {
-		double area = 0.0;
-		
+        double area = 0.0;
+
         return area;
     }
 
     /**
      * [7b] titik p di dalem ato luar,
-     * jika berimpitan dengan titik atau segmen, di dalam 
+     * jika berimpitan dengan titik atau segmen, di dalam
+     * 
      * @param p
      * @return
      */
     boolean isPointInside(MyPoint p) {
-		boolean inside = true;
-		
+        boolean inside = true;
+        for (int i = 0; i < this.Points.size() && inside == true; i++) {
+            MyPoint p1 = Points.get(i);
+            MyPoint p2 = Points.get((i + 1) % Points.size());
+            MyLineSegment vector = new MyLineSegment(p1, p2);
+            double direction = vector.leftTurnToPoint(p);
+            if (direction == 0) {
+                if (vector.distanceToPoint(p) != 0)
+                    inside = false;
+            } else if (direction > 0)
+                inside = false;
+        }
         return inside;
     }
 
